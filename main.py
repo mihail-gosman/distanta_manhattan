3613bb3ebba1f3ec955ca9657793375b415aebc1
mihail-gosman <116556335+mihail-gosman@users.noreply.github.com>
2026-03-05 10:19:35 +0200

    Refactor Manhattan distance and file loading logic

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-def distanta_manhattan(v1, v2):
-    suma = 0
-    for i in range(len(v1)):
-        suma += abs(v1[i] - v2[i])
-    return suma
+def distanta_manhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-f = open("date.txt", "r")
-linii = f.readlines()
-f.close()
+def incarca_date(nume_fisier):
+    puncte = []
+    f = open(nume_fisier, "r")
+    for linie in f:
+        coordonate = linie.strip().split(",")
+        x = int(coordonate[0])
+        y = int(coordonate[1])
+        puncte.append([x, y])
+    f.close()
+    return puncte
 
-# Transformam liniile din fisier in liste de numere (vectori)
-vector1 = [int(x) for x in linii[0].strip().split(",")]
-vector2 = [int(x) for x in linii[1].strip().split(",")]
-
-rezultat = distanta_manhattan(vector1, vector2)
-
-# Chestie de securitate: prag de alerta
-prag_alerta = 100
-
-if rezultat > prag_alerta:
-    print(f"ALERTA: Diferenta de {rezultat} depaseste pragul de securitate!")
-else:
-    print(f"Sistem stabil. Distanta Manhattan: {rezultat}")
+if __name__ == "__main__":
+    date = incarca_date("date.txt")
+    
+    if len(date) >= 2:
+        rezultat = distanta_manhattan(date[0], date[1])
+        print("Distanta Manhattan este:", rezultat)
+        
+        # Prag de securitate
+        if rezultat > 100:
+            print("Alerta: Anomalie detectata!")
+    else:
+        print("Eroare: Fisierul trebuie sa contina cel putin doua puncte.")
